68f1702ff07af557355b13e0640fcd0a5d0d062e
Clement Soulies <clement.soulies@egenius2.fr>
2023-10-10 14:49:07 +0200

    addcallkeep

--- App.tsx
+++ App.tsx
@@ -8,8 +8,8 @@ window.navigator.mediaDevices = window.navigator.mediaDevices || mediaDevices;
 window.navigator.getUserMedia =
   window.navigator.getUserMedia || mediaDevices.getUserMedia;
 
-import React from 'react';
-import {Button, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Button, PermissionsAndroid, Text, View} from 'react-native';
 import {
   mediaDevices,
   MediaStream,
@@ -19,8 +19,34 @@ import {
   RTCSessionDescription,
 } from 'react-native-webrtc';
 import Sip from '@khateeb00/react-jssip';
+import RNCallKeep from 'react-native-callkeep';
+
+const options = {
+  ios: {
+    appName: 'My app name',
+    supportsVideo: false,
+
+  },
+  android: {
+    additionalPermissions: [PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,],
+    alertTitle: 'Permissions required',
+    alertDescription: 'This application needs to access your phone accounts',
+    cancelButton: 'Cancel',
+    okButton: 'ok',
+  },
+};
+RNCallKeep.setup(options);
 
 const App = () => {
+  const [callUUID, setCallUUID] = useState('');
+  const [callingNumber, setCallingNumber] = useState('');
+  const [logText, setLog] = useState('');
+  const [heldCalls, setHeldCalls] = useState({}); // callKeep uuid: held
+  const [mutedCalls, setMutedCalls] = useState({}); // callKeep uuid: muted
+  const [calls, setCalls] = useState({}); // callKeep uuid: number
+
+
   Sip.register({
     websocket: 'wss://freeswitch1.your-avatar.eu:7443',
     username: 'jlessart',
@@ -29,11 +55,37 @@ const App = () => {
     name: 'Jlessart',
   });
 
+  const eventhandler = Sip.on('call_received', e => {
+    console.log('la');
+
+    let uid = e._request.call_id;
+    setCallingNumber(e._remote_identity._uri._user);
+    setCallUUID(uid);
+    RNCallKeep.displayIncomingCall(
+      e._request.call_id,
+      'toto',
+      'utilisateur certifie ' + e._remote_identity._uri._user,
+      'number',
+    );
+    console.log('passe Ici');
+  });
+
   const call = () => {
     Sip.makeCall('sip:0619150747@avatar.com').then(data => {
       console.log(data);
     });
   };
+
+  useEffect(() => {
+    RNCallKeep.setAvailable(true);
+    eventhandler;
+    // @ts-ignore
+    // RNCallKeep.addEventListener('answerCall', onAnswerCallAction);
+      return () => {
+      // RNCallKeep.removeEventListener('answerCall', onAnswerCallAction);
+    };
+  }, []);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>App</Text>
